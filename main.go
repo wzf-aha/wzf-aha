2168961a487b4338ca216e2dc599b94b4d083196
万志方 <wanzhifang@limikeji.com>
2021-04-26 00:37:00 +0800

    第二周作业

--- main.go
+++ main.go
@@ -58,14 +58,15 @@ func main()  {
 
 /**
  *  在获取单行数据时，Scan没有匹配到结果集时会报sql.ErrNoRows错误
- *	dao 层中当遇到一个 sql.ErrNoRows 的时候，应该Wrap 这个 error，抛给上层
+ *	dao 层中当遇到一个 sql.ErrNoRows 的时候，不应该Wrap 这个 error，抛给上层
  *  dao 层只负责查询或操作数据，不对结果做处理，错误和结果应由上层的业务逻辑来判断是否要处理及怎么处理
+ *	dao 层具有比较高的重用性，最好返回根错误供业务侧判别
  */
 func TestSelect(db *sql.DB) (year int,err error) {
 	err = db.QueryRow("select year from books where id = ?", 1).Scan(&year)
-	if err != nil {//添加上能帮助定位问题或者问题数据的信息往上抛
-		return 0, errors.Wrap(err,"err id:1")
-	}
+	//if err != nil {//添加上能帮助定位问题或者问题数据的信息往上抛
+	//	return 0, errors.Wrap(err,"err id:1")
+	//}
 	return
 }
 
